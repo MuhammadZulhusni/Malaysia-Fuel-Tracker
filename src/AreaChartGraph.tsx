832c9d1f8a6b0378336a85e8bc3de12156b5893c
zulhusni2003 <zulhusnifamile@gmail.com>
2024-05-22 22:16:01 +0800

    Made graph responsive for all devices

--- src/AreaChartGraph.tsx
+++ src/AreaChartGraph.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area, Legend } from "recharts";
+import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area, Legend, ResponsiveContainer } from "recharts";
 
 interface Props {
   filteredData: any[]; // Define prop type for filtered data
@@ -16,34 +16,36 @@ const AreaChartGraph: React.FC<Props> = ({ filteredData, title, explanation }) =
       {/* Title and explanation */}
       <h2 className="text-4xl font-bold text-blue-600 bg-gradient-to-r from-blue-900 to-blue-600 bg-clip-text text-transparent">{title}</h2>
       <p className="text-gray-600 mt-3">{explanation}</p>
-      <div className="flex justify-center mt-3">
-        {/* Area chart component */}
-        <AreaChart width={800} height={400} data={filteredData}>
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#FFB74D" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#FFB74D" stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#81C784" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#81C784" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          {/* X axis */}
-          <XAxis dataKey="date" tickFormatter={(date) => new Date(date).getFullYear().toString()} /> {/* Convert year to string */}
-          <YAxis />
-          <YAxis domain={[0, "auto"]} /> {/* Auto adjust y-axis domain */}
-          {/* Grid */}
-          <CartesianGrid strokeDasharray="1 1" />
-          {/* Tooltip */}
-          <Tooltip />
-          {/* Areas */}
-          <Area type="monotone" dataKey="ron95" stroke="#FFB74D" fillOpacity={0.8} fill="url(#colorUv)" name="RON95" />
-          <Area type="monotone" dataKey="ron97" stroke="#81C784" fillOpacity={0.8} fill="url(#colorPv)" name="RON97" />
-          <Area type="monotone" dataKey="diesel" stroke="#333333" fillOpacity={0.8} fill="url(#colorUv)" name="DIESEL" />
-          {/* Legend */}
-          <Legend />
-        </AreaChart>
+      <div className="flex justify-center mt-3 w-full" style={{ maxWidth: "100%", height: 400 }}>
+        {/* Responsive container for area chart */}
+        <ResponsiveContainer>
+          <AreaChart data={filteredData}>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#FFB74D" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#FFB74D" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#81C784" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#81C784" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            {/* X axis */}
+            <XAxis dataKey="date" tickFormatter={(date) => new Date(date).getFullYear().toString()} /> {/* Convert year to string */}
+            <YAxis />
+            <YAxis domain={[0, "auto"]} /> {/* Auto adjust y-axis domain */}
+            {/* Grid */}
+            <CartesianGrid strokeDasharray="1 1" />
+            {/* Tooltip */}
+            <Tooltip />
+            {/* Areas */}
+            <Area type="monotone" dataKey="ron95" stroke="#FFB74D" fillOpacity={0.8} fill="url(#colorUv)" name="RON95" />
+            <Area type="monotone" dataKey="ron97" stroke="#81C784" fillOpacity={0.8} fill="url(#colorPv)" name="RON97" />
+            <Area type="monotone" dataKey="diesel" stroke="#333333" fillOpacity={0.8} fill="url(#colorUv)" name="DIESEL" />
+            {/* Legend */}
+            <Legend />
+          </AreaChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
