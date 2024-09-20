7815aa56c61b8c7154dc89d7b7254705fcefd89b
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2024-09-20 22:26:25 +0800

    Understanding code: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Initialize the root of the React application and render it into the DOM element with id 'root'
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    {/* Wrap the App component in React.StrictMode to highlight potential problems */}
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
